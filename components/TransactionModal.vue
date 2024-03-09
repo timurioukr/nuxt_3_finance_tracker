1dff85d1209b6f9e63a1bf98c200cd3406816284
Timur Shekhovtsov <timurioss@MacBook-Pro-Timur.local>
2024-03-09 23:47:17 +0200

    add form

---
+++ components/TransactionModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <UModal v-model="isOpen">
+    <UCard>
+      <template #header> Add transaction </template>
+
+      <UForm :state="state">
+        <UFormGroup :required="true" label="Transaction type" name="type" class="mb-4">
+        <USelect placeholder="Select the transaction type" :options="types" />
+      </UFormGroup>
+
+      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+        <UInput type="number" placeholder="Amount" />
+      </UFormGroup>
+      <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
+        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
+      </UFormGroup>
+      <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
+        <UInput placeholder="Description" />
+      </UFormGroup>
+      <UFormGroup :required="true" label="Category" name="category" class="mb-4">
+        <USelect placeholder="Description" :options="categories" />
+      </UFormGroup>
+
+      <UButton type="submit" color="black" variant="solid" label="Save"/>
+      </UForm>
+    </UCard>
+  </UModal>
+</template>
+
+<script setup>
+import { categories, types } from "~/constants"
+
+const props = defineProps({
+  modelValue: Boolean
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const state = ref({
+  type: undefined,
+  amount: 0,
+  created_at: undefined,
+  description: undefined,
+  category: undefined
+})
+
+const isOpen = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value)
+})
+
+</script>
--- constants.js
+++ constants.js
@@ -1,1 +1,3 @@
 export const transactionViewOptions = ['Yearly', 'Monthly', 'Daily']
+export const categories = ['Food', 'Houseng', 'Car']
+export const types = ['Income', 'Expense', 'Saving', 'Investment']
