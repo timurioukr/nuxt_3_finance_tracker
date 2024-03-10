1fe177a63a7f4b233482269b50db4c0abcfc8f92
Timur Shekhovtsov <timurioss@MacBook-Pro-Timur.local>
2024-03-10 19:57:01 +0200

    add form

--- components/TransactionModal.vue
+++ components/TransactionModal.vue
@@ -3,22 +3,22 @@
     <UCard>
       <template #header> Add transaction </template>
 
-      <UForm :state="state">
+      <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
         <UFormGroup :required="true" label="Transaction type" name="type" class="mb-4">
-        <USelect placeholder="Select the transaction type" :options="types" />
+        <USelect placeholder="Select the transaction type" :options="types" v-model="state.type"/>
       </UFormGroup>
 
       <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-        <UInput type="number" placeholder="Amount" />
+        <UInput type="number" placeholder="Amount" v-model.number="state.amount"/>
       </UFormGroup>
       <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
-        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
+        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
       </UFormGroup>
-      <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
+      <UFormGroup label="Description" hint="Optional" name="description" class="mb-4" v-model="state.description">
         <UInput placeholder="Description" />
       </UFormGroup>
-      <UFormGroup :required="true" label="Category" name="category" class="mb-4">
-        <USelect placeholder="Description" :options="categories" />
+      <UFormGroup :required="true" label="Category" name="category" class="mb-4" v-if="state.type === 'Expense'">
+        <USelect placeholder="Description" :options="categories" v-model="state.category"/>
       </UFormGroup>
 
       <UButton type="submit" color="black" variant="solid" label="Save"/>
@@ -29,6 +29,7 @@
 
 <script setup>
 import { categories, types } from "~/constants"
+import { z } from "zod"
 
 const props = defineProps({
   modelValue: Boolean
@@ -36,17 +37,61 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue'])
 
-const state = ref({
+const defaultSchema = z.object({
+  created_at: z.string(),
+  description: z.string().optional(),
+  amount: z.number().positive('Amount must to be more than 0')
+})
+
+const incomeSchema = z.object({
+  type: z.literal('Income')
+})
+const expenseSchema = z.object({
+  type: z.literal('Expense'),
+  category: z.enum(categories)
+})
+const investmentSchema = z.object({
+  type: z.literal('Investment')
+})
+const savingSchema = z.object({
+  type: z.literal('Saving'),
+})
+
+const schema = z.intersection(
+  z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
+  defaultSchema
+)
+
+const form = ref()
+
+const save = async () => {
+  if (form.value.errors.length) return
+  form.value.validate()
+}
+
+const initialState = {
   type: undefined,
   amount: 0,
   created_at: undefined,
   description: undefined,
   category: undefined
+}
+
+const state = ref({
+  ...initialState
 })
+
+const resetForm = () => {
+  Object.assign(state.value, initialState)
+  form.value.clear()
+}
 
 const isOpen = computed({
   get: () => props.modelValue,
-  set: (value) => emit('update:modelValue', value)
+  set: (value) => {
+    if (!value) resetForm()
+    emit('update:modelValue', value)
+  }
 })
 
 </script>
